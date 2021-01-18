90960396a65955f0db03dacf3beaf980e38d4b2a
spin03 <tanwirayut@gmail.com>
2021-01-18 11:14:47 +0700

    orm

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,13 +1,23 @@
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
 
+const config = dotenv.config({
+  path: join(__dirname, '..', '.env'),
+});
 
-export const orm_config: TypeOrmModule = {
-    "type": "postgres",
-    "host": "localhost",
-    "port": 5432,
-    "username": "postgres",
-    "password": "1987",
-    "database": "productdata",
-    "entities": ["dist/**/*.entity{.ts,.js}"],
-    "synchronize": true
+if (config.error) {
+  throw config.error;
 }
+
+export const orm_config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  synchronize: true,
+  logging: false,
+};
